c2e9260b4a24cd859b4c52ec3f02779a94f567ad
Aaron Fath <aaron.fath@stud.uni-heidelberg.de>
2026-02-21 00:32:11 +0100

    implemented OPTICS (lacks results yet)

--- Haufenbildung/R/OPTICS.R
+++ Haufenbildung/R/OPTICS.R
@@ -2,13 +2,13 @@
 
 optics <- function(data, epsilon, min_Pts) {
   x <- as.matrix(data)    # data will be created by generateClusterTestDataSimple2D() or similiar function
-  dist_x <- as.matrix(stats::dist(x)) # die Distanzmatrix von data
-  data_entries <- nrow(x)
+  dist_x <- as.matrix(stats::dist(x)) # die Distanzmatrix von data, verbessern wir möglicherweise noch
+  data_entries <- nrow(x) # number of points
   
   processed <- rep(FALSE, data_entries)
   reachability <- rep(NA, data_entries)
   core_distance <- rep(NA, data_entries)
-  ordered <- 1L
+  ordered <- integer(0)
   
   getNeighbors <- function(point) {
     which(dist_x[point, ] <= epsilon) # Liste an Punkten, die innerhalb von Distanz epsilon um Punkt point liegen, point inklusive
@@ -16,50 +16,78 @@ optics <- function(data, epsilon, min_Pts) {
   
   coreDistance <- function(point, neighbors) {
     if(length(neighbors) < min_Pts) {
-      return NA
+      return(NA)
     }
     dists <- dist_x[point, neighbors]
     return(sort(dists)[min_Pts])
   }
   
-  for(point in 1:data_entries) {  # for each point in data
-    # point.reachability-distance = NULL
-    if(processed[point] == FALSE) {   # for each unprocessed point in data
-      processed[point] <- TRUE  # mark point as processed
-      neighbors <- getNeighbors(point) # N <- getNeighbors(point, epsilon)
-      ordered <- c(ordered, point)     # place point in ordered list
-      core_distance[point] <- coreDistance(point, neighbors)
-      
-      if(!is.na(coreDistance(point, neighbors))) {    # if (core-distance(point, epsilon, min_Pts) != NULL) 
-        Seeds <- list(points = integer(0), reachability = numeric(0)) # Seeds = empty priority queue
-        
+  update <- function(neighbors, point, seeds_points, seeds_reach) {
+    coredist <- core_distance[point]
+    for (o in neighbors) { # for each o in N
+      if(processed[o] == FALSE) { # if (o = unprocessed)
+        new_reach <- max(coredist, dist_x[point, o]) # new-reach-dist = max(coredist, dist(p, o))
+        if(is.na(reachability[o])) { # if (o.reachability-distance = NULL)
+          # o not in Seeds:
+          reachability[o] <<- new_reach #o.reachability-distance = new-reach-dist
+          seeds_points <- c(seeds_points, o) # Seeds.insert(o, new-reach-dist)
+          seeds_reach <- c(seeds_reach, new_reach)
+        } else if(new_reach < reachability[o]) { #else: o in Seeds, check improvement// if (new-reach-dist < o.reachability-distance)
+          reachability[o] <<- new_reach # o.reachability-distance = new-reach-dist
+          # Seeds.move-up(o, new-reach-dist)
+          i <- which(seeds_points == o)
+          if(length(i) > 0) {
+            seeds_reach[i] <- new_reach
+          }
+        }
       }
     }
-    
-    return(list(order = ordered, reachability = reachability, core_distance = core_distance))
+  list(points = seeds_points, reach = seeds_reach)
   }
-
+  
+  for(point in 1:data_entries) {  # for each point in data
+    # point.reachability-distance = NULL
+    # we can save one more for loop? maybe? 
+    if(processed[point] == FALSE) {   # for each unprocessed point in data
+      neighbors <- getNeighbors(point) # N <- getNeighbors(point, epsilon)
+      processed[point] <- TRUE  # mark point as processed
+      ordered <- c(ordered, point)     # place point in ordered list
+      core_distance[point] <- coreDistance(point, neighbors)
+  
+      if(!is.na(core_distance[point])) {    # if (core-distance(point, epsilon, min_Pts) != NULL) 
+        # define Seeds, as "priority queue" doesn't exist in base R
+        seeds_points <- integer(0)
+        seeds_reach <- numeric(0)
+        
+        seeds <- update(neighbors, point, seeds_points, seeds_reach) # update(N, point, Seeds, epsilon, min_Pts)
+        seeds_points <- seeds$points
+        seeds_reach <- seeds$reach
+        
+        while (length(seeds_points) > 0) { # for each next q in Seeds
+          i <- which.min(seeds_reach)
+          q <- seeds_points[i]
+          
+          seeds_points <- seeds_points[-i]
+          seeds_reach <- seeds_reach[-i]
+          
+          if(processed[q] == FALSE) {
+            neighbors_q <- getNeighbors(q) # N' = getNeighbors(q, epsilon)
+            processed[q] <- TRUE # mark q as processed
+            ordered <- c(ordered, q) # place q in ordered list
+            core_distance[q] <- coreDistance(q, neighbors_q)
+            if(!is.na(core_distance[q])) { # if (core-distance(q, epsilon, min_Pts) != NULL)
+              seeds <- update(neighbors_q, q, seeds_points, seeds_reach) # update(N', q, Seeds, epsilon, min_Pts)# update(N', q, Seeds, epsilon, min_Pts)              
+              seeds_points <- seeds$points
+              seeds_reach <- seeds$reach
+            }
+          }
+        }
+      }
+    }
+  }
+  return(list(order = ordered, reachability = reachability, core_distance = core_distance))
     
-
-      # update(N, point, Seeds, epsilon, min_Pts)
-      # for each next q in Seeds
-      # N' = getNeighbors(q, epsilon)
-      # mark q as processed
-      # place q in ordered list
-      # if (core-distance(q, epsilon, min_Pts) != NULL)
-        # update(N', q, Seeds, epsilon, min_Pts)
 }
 
-update <- function(N, p, Seeds, epsilon, min_Pts) {
-  coredist <- core-distance(p, epsilon, min_Pts)
-  # for each o in N
-    # if (o = unprocessed)
-      # new-reach-dist = max(coredist, dist(p, o))
-      # if (o.reachability-distance = NULL) o not in Seeds
-        # o.reachability-distance = new-reach-dist
-        # Seeds.insert(o, new-reach-dist)
-    # else      o in Seeds, check improvement
-      # if (new-reach-dist < o.reachability-distance)
-        # o.reachability-distance = new-reach-dist
-        # Seeds.move-up(o, new-reach-dist)
-}
+# not looking at the results today, we believe this is correct <3
+# es fehlen noch reachability-graph und clustering-graph, das kommt noch!
