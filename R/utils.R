1c3dfd175ad0ea6fcbaebc2bc4f63e38231723fc
adamfuge2 <adamfuge.fachschaft@gmail.com>
2026-02-24 00:15:05 +0100

    up to date viewClusters documetation

--- R/utils.R
+++ R/utils.R
@@ -344,6 +344,24 @@ sumOfDistancestTo <- function(data,vector,metric){
 ################# Viewers ######################
 
 
+#' View 2D cluster data
+#'
+#' Displays 2D data points color coded by their cluster. Can be used for:
+#' \itemize{
+#'    \item \strong{data without clusters}, in this case don't input a clustering function and make sure your data \strong{does not} have a column \code{'cluster'}.
+#' }Currently only works for clusters represented by numbers >=
+#' 0. Every data point in 'cluster' will be labeled as an 'Outlier'.
+#'
+#' @param clusterd_data   A tibble with every row representing a data point.
+#'   Must contain a column called 'cluster' in which the cluster belonging to
+#'   the respective data point is stored.
+#'
+#' @examples
+#' data_2D <- generateClusterTestDataSimple(dim=2,cluster_amount=5)
+#' clustering <- K_means(data_2D,K=5)
+#' clustered_data_2D <- data_2D |> dplyr::rowwise() |> dplyr::mutate(cluster=clustering(dplyr::c_across(everything())))
+#' viewClusters2D(clustered_data_2D)
+#'
 viewClusters2D <-function(clustered_data){
 
   dim <- ncol(clustered_data)-1
@@ -376,10 +394,30 @@ viewClusters2D <-function(clustered_data){
 
 
 
-
+#' View 3D cluster data
+#'
+#' Displays 3D data points color coded by their cluster. Can be used for:
+#' \itemize{
+#'    \item \strong{data without clusters}, in this case don't input a clustering function and make sure your data \strong{does not} have a column \code{'cluster'}.
+#' }Currently only works for clusters represented by numbers >=
+#' 0. Every data point in 'cluster' will be labeled as an 'Outlier'.
+#'
+#' @param clusterd_data   A tibble with every row representing a data point.
+#'   Must contain a column called 'cluster' in which the cluster belonging to
+#'   the respective data point is stored.
+#'
+#' @examples
+#' data_3D <- generateClusterTestDataSimple(dim=3,cluster_amount=5)
+#' clustering <- K_means(data_3D,K=5)
+#' clustered_data_3D <- data_3D |> dplyr::rowwise() |> dplyr::mutate(cluster=clustering(dplyr::c_across(everything())))
+#' viewClusters3D(clustered_data_3D)
+#'
 viewClusters3D <-function(clustered_data){
   dim <- ncol(clustered_data)-1
+  stopifnot('clustered_data must have a row called \'cluster\''= 'cluster'%in%colnames(clustered_data))
   stopifnot('viewClusters3D can only display clusterings of 3D data' = dim==3)
+
+
 
   prefered_colnames <- c('X','Y','Z')
   colname_index <- 1
@@ -410,13 +448,46 @@ viewClusters3D <-function(clustered_data){
   scatterplot3d::scatterplot3d(x,y,z,color = color,pch = 16)
 }
 
-#' View clustered data
+#' View cluster data
 #'
-#' acts as a wrapper for view_data() in the case of unclustered data
+#' Displays 1D to 3D data points color coded by their cluster. Can be used for:
+#' \itemize{
+#'  \item \strong{data and an applicable clustering function}.
+#'  \item \strong{already clustered data},  in this case don't input a clustering function and make sure your data has a column \code{'cluster'}.
+#'  \item \strong{data without clusters}, in this case don't input a clustering function and make sure your data \strong{does not} have a column \code{'cluster'}.
+#' }
+#' Acts as a wrapper for viewClusters2D, viewClusters3D, viewData, viewData2D
+#' and viewData3D.\n
+#' Currently only works for clusters represented by numbers >= 0. Every data point
+#' in 'cluster' will be labeled as an 'Outlier'.
 #'
-#' @param data        a tibble with every row representing a data point.
-#' @param clustering  a clustering function applicable to the data.
-#'    If none given, the data will be displayed without clusters, wraps view_data().
+#' @param data        a tibble with every row representing a data point. If \code{data}
+#'   has a column named \code{'cluster'}, then this column will be used to color
+#'   the data points
+#' @param clustering  a clustering function applicable to the data. If none
+#'   given, the data will be displayed with the clusters deferred from the
+#'   column \code{'cluster'} of data or without clusters, using
+#'   \code{viewData(data)}.
+#'
+#' @examples
+#' # 1D data without clusters
+#' data_1D <- generateClusterTestDataSimple(dim=1)
+#' viewClusters(data_1D)
+#' # 2D data without clusters
+#' data_2D <- generateClusterTestDataSimple(dim=2)
+#' viewClusters(data_2D)
+#' # 3D data without clusters
+#' data_3D <- generateClusterTestDataSimple(dim=3)
+#' viewClusters(data_3D)
+#' # 3D data with a clustering function
+#' clustering <- K_means(data_3D,K=5)
+#' viewClusters(data_3D,clustering)
+#' # 2D data with column 'cluster'
+#' clustering <- K_means(data_2D,K=5)
+#' clustered_data_2D <- data_2D |> dplyr::rowwise() |> dplyr::mutate(cluster=clustering(dplyr::c_across(everything())))
+#' viewClusters(clustered_data_2D)
+#'
+#'
 #' @export
 viewClusters <- function(data,clustering=NULL){
 
@@ -453,10 +524,14 @@ viewClusters <- function(data,clustering=NULL){
 }
 
 
-#' View 2D data as a simple scatter plot
+#' View 2D data as scatter plot
 #'
-#' Inputs:
-#' @param data        a tibble with every row representing a data point.
+#' @param data   A tibble with every row representing a data point.
+#'
+#' @examples
+#' data_3D <- generateClusterTestDataSimple(dim=3,cluster_amount=5)
+#' viewData3D(data_3D)
+#'
 viewData2D <- function(data){
   colnames(data) <- c('X','Y')
 
@@ -464,12 +539,42 @@ viewData2D <- function(data){
     ggplot2::geom_point()
 }
 
-viewData3D <- function(data){
-  colnames(data) <- c('X','Y','Z')
 
-  scatterplot3d::scatterplot3d(data,color = 'black')
+#' View 3D data as scatter plot
+#'
+#' @param data   A tibble with every row representing a data point.
+#'
+#' @examples
+#' data_2D <- generateClusterTestDataSimple(dim=2,cluster_amount=5)
+#' viewData2D(data_2D)
+#'
+viewData3D <- function(data){
+
+  x <- data[[1]]
+  y <- data[[2]]
+  z <- data[[3]]
+
+  scatterplot3d::scatterplot3d(x,y,z,color = 'black')
 }
 
+#' View data as scatter plot
+#'
+#' Displays 1D to 3D data points color.\n
+#' Acts as a wrapper for viewData2D and viewData3D.
+#'
+#' @param data   A tibble with every row representing a data point.
+#'
+#' @examples
+#' # 1D data without clusters
+#' data_1D <- generateClusterTestDataSimple(dim=1)
+#' viewData(data_1D)
+#' # 2D data without clusters
+#' data_2D <- generateClusterTestDataSimple(dim=2)
+#' viewData(data_2D)
+#' # 3D data without clusters
+#' data_3D <- generateClusterTestDataSimple(dim=3)
+#' viewData(data_3D)
+#'
 #' @export
 viewData <- function(data){
   dim <- base::ncol(data)
@@ -491,9 +596,27 @@ viewData <- function(data){
 
 
 
-clusterLabeling <- function(x){
-  if(x==0) return('Outlier')
-  return(paste0('Cluster ',x))
+#' Derive cluster label from cluster number
+#'
+#' Helper function.\n
+#' Converts an identification (generally integers) of a cluster to a more descriptive cluster label.
+#' The 0th cluster gets labeled as the Outliers cluster.
+#'
+#' @param n A clusters identification. Can be a numeric or character.
+#'
+#' @returns A character like \code{'Cluster 15'}, or \code{'Outlier'} in case n is zero.
+#'
+#' @examples
+#' # example code
+#' clusterLabeling(36)
+#' clusterLabeling('siebzehn')
+#' clusterLabeling(0)
+#' clusterLabeling('0')
+#'
+#'
+clusterLabeling <- function(n){
+  if(is.na(n) || n==0 || n=='0' || is.null(n)) return('Outlier')
+  return(paste0('Cluster ',n))
 }
 
 
