e6620a249e83465313125851d0b5b1fca3c675c3
adamfuge2 <adamfuge.fachschaft@gmail.com>
2026-02-26 16:21:42 +0100

    some updates to spectral reduction function

--- R/spectral-clustering.R
+++ R/spectral-clustering.R
@@ -32,30 +32,56 @@ t(eig$vectors[, ncol(eig$vectors):1])[2:3,]
 
 
 #as tibbles:
-gaussKernel <- function(data){
+gaussKernelWeights <- function(data,gamma){
   base::exp(- gamma * base::as.matrix(stats::dist(data)))
 }
 
-spectralReduction <- function(data, gamma, k, kernel = gaussKernel, metric = NULL, epsilon = NULL){
+spectralReduction <- function(data, gamma, k, mercian_kernel = 'gauss', custom_mercian_kernel = NULL, metric = NULL, epsilon = NULL){
   n <- nrow(data)
-  W <- kernel(data)
-  D <- matrix(0, nrow = n, ncol = n)
 
-  #change into |> apply
-  for (i in seq(n)){
-    D[i,i] <- W[,i] |> sum()
+
+  if(missing(custom_mercian_kernel)){
+    if(mercian_kernel == 'gauss') W <- gaussKernelWeights(data,gamma)
+    else stop('Unknown mercian kernel and no custom mercian kernel provided')
   }
+  else {
+    W <- dissimilarityMatrix(data,custom_mercian_kernel)
+  }
+
+
+  D <- apply(W,1,sum) |> diag()
 
   L <- D - W
 
   D_sqrt <- 1/sqrt(D)
   D_sqrt[D_sqrt == Inf] <- 0
 
-  eigenvectors <- eigen(D_sqrt %*% L %*% D_sqrt, symmetric=TRUE)$vectors
-  t(eigenvectors[, n:1])[2:(k+1), ] |>
-    t() |>
-    tibble::as_tibble()
+  # calculate eigenvectors
+  b <- eigen(D_sqrt %*% L %*% D_sqrt, symmetric=TRUE)$vectors[,n:1]
 
+  # transform to be solutions of our optiization problem
+  beta <- apply(b,c(2),function(x) n^(-1/2)*D_sqrt %*% x)
+
+  # retrieve the new transformed data points (columns of a).
+  # The eigenvector belonging to the lowest eigenvalue gets left out, as it is
+  # linearly dependent to (1,1, ... ,1) and thus bears no information.
+  a <- t(beta)[2:(k+1), ]
+
+
+  # prevent conversion to atomic vector, if k = 1
+  dim(a) <- c(k,n)
+
+  # save the data points in tibble as usual
+  reduced_data <- tibble::as_tibble(t(a))
+
+  # useful names
+  colnames(reduced_data) <- paste0('X_',1:k)
+
+  return(list(reduced_data = reduced_data, transform_function = function(...) stop('not implemented yet')))
 }
 
+data <- generateClusterTestDataSimple(n=100,dim=3,cluster_amount = 2)
 
+viewClusters(data)
+viewClusters(spectralReduction(data,gamma=5,k=1)$reduced_data)
+viewClusters(reduced_data)
