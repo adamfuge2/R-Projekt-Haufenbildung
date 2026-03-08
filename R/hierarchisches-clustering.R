177c6539b1038844a4507867fc53eccc4a7d604c
Roman Hector <roman.hector@stud.uni-heidelberg.de>
2026-03-08 20:43:31 +0100

    bringing hierarchical clustering to compatible form on own branch

--- R/hierarchisches-clustering.R
+++ R/hierarchisches-clustering.R
@@ -10,14 +10,42 @@
 ##
 ## Returns:
 ## res        a tibble of same length as data with an entry 'cluster'
-hierarchical_clustering <- function(data, n, mode = centroid, metric = euclidean){
-  data <- tibble::rowid_to_column(data, "cluster")   #assign each datapoint a cluster-ID
+hierarchical_clustering <- function(data, n, mode = "centroid", metric = "euclidean", p = NULL, custom_metric = NULL){
+  data <- tibble::rowid_to_column(tibble::tibble(data), "cluster")   #assign each datapoint a cluster-ID
+  dim <- base::ncol(data) - 1
+
+  # metric selection
+  if(is.null(custom_metric)){
+    if(metric=='euclidean')
+      metric <- euclidean
+    else if(metric=='maximum')
+      metric <- maximumMetric
+    else if(metric=='Lp'){
+      stopifnot('If you chose the Lp metric, please provide a value for p' = !is.null(p))
+      stopifnot('p must be a numeric greater than or equal to 1' = is.numeric(p) && p>=1 )
+      metric <- pMetric(p)
+    }
+    else if(metric=='manhattan')
+      metric <- pMetric(1)
+    else stop('Unknown metric. Look up on the help page which metrics are available ore input a custum metric using the argument custom_metric.')
+  }
+  else metric <- custom_metric
+
+  # mode selection
+  if(mode == "centroid")
+    mode <- centroid
+  else if(mode == "single")
+    mode <- single
+  else if(mode == "complete")
+    mode <- complete
+  else if(mode == "average")
+    mode <- average
+  else stop("Unknown linkage mode. Currently implemented selection includes: centroid, single, complete, average")
 
   while(data$cluster |> unique() |> length() > n){
     min_dist <- Inf   #minimal distance between two points
     neighbors <- c(0,0)
-    cluster <- data$cluster |> unique()
-
+    cluster <- data$cluster |> base::unique()
     for (i in cluster){
       for (j in cluster[cluster != i]){
         tbl1 <- dplyr::filter(data, cluster == i) |> dplyr::mutate(cluster=NULL)  #remove the 'cluster' entry (otherwise it would add to the distance)
@@ -32,5 +60,43 @@ hierarchical_clustering <- function(data, n, mode = centroid, metric = euclidean
     data <- data |> dplyr::mutate(cluster = ifelse(cluster == neighbors[2], neighbors[1], cluster )) #assign all datapoints within the merged cluster the same id
   }
   data <- dplyr::mutate(data, cluster = dplyr::dense_rank(cluster))
-  return(data)
+
+
+  return(data) #### remove
+
+  centroids <- data |>
+    dplyr::ungroup() |>
+    dplyr::group_by(cluster) |>
+    dplyr::summarise_at(1:dim,mean) |>
+    dplyr::select(-cluster)
+
+  return(structure(
+    list(
+      clustered_data = data,
+      clustering_function = function(x)
+        centroids |>
+        apply(1,function(centroid) metric(x,centroid)) |>
+        base::which.min(),
+      inner_inequality = ),
+    description = 'Data clustered by hierarchical clustering algorithm',
+    class = 'clustering'
+    )
+  )
 }
+
+
+df <- generateClusterTestDataSimple(n = 20, dim = 3)
+data <- hierarchical_clustering(df, 3)
+
+dim <- 3
+centroids <- data |>
+  dplyr::ungroup() |>
+  dplyr::group_by(cluster) |>
+  dplyr::summarise_at(1:dim,mean) |>
+  dplyr::select(-cluster)
+
+metric <- euclidean
+distances <- centroids |> apply(1,function(centroid){data[,1:dim] |> apply(1,function(x){metric(x,centroid)})}) |> t()
+
+distances <- data |> dplyr::mutate(dist = metric())
+distances
