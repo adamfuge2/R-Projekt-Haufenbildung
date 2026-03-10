b1230c45fd7b2bbe5207df86ee2fa2c4d54f5987
adamfuge2 <adamfuge.fachschaft@gmail.com>
2026-03-10 01:08:07 +0100

    updated data

--- data-raw/three_connected_concentric_circles.R
+++ data-raw/three_connected_concentric_circles.R
@@ -4,9 +4,9 @@ circle <- function(r) tibble::tibble(X=c(r,r/sqrt(2),0,-r/sqrt(2),-r,-r/sqrt(2),
 # combine those points to one list of paths
 # Note that we include the larger circle more often,
 # which causes the density of the clusters to be about equal
-connected_circles_paths <- list(circle(0.3),circle(0.3),circle(0.3),circle(0.3),
-                                circle(0.6),circle(0.6),circle(0.6),circle(0.6),circle(0.6),circle(0.6),circle(0.6),circle(0.6),
-                                circle(1),circle(1),circle(1),circle(1),circle(1),circle(1),circle(1),circle(1),circle(1),circle(1),circle(1),circle(1),circle(1),circle(1),circle(1),circle(1),circle(1),circle(1),
+connected_circles_paths <- list(circle(0.3),
+                                circle(0.6),
+                                circle(1),
                                 tibble::tibble(X=c(0,0),Y=c(0.3,1)))
 
 # generating the resulting data set
